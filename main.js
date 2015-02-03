b8487c0dfeaf3a18c7ab38c4712c652f0f2e07d3
Martin Zagora <zaggino@gmail.com>
2015-02-04 10:19:04 +1100

    make sure es6 extension is recognized by Brackets

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-/*global define*/
+/*global brackets, define*/
 
 define(function (require) {
     "use strict";
+
+    // register es6 as javascript file extension in Brackets
+    var LanguageManager = brackets.getModule("language/LanguageManager");
+    if (LanguageManager.getLanguageForExtension("es6") == null) {
+        LanguageManager.getLanguageForExtension("js").addFileExtension("es6");
+    }
+
     require("dist/browser-polyfill");
     require("dist/main")();
 });
