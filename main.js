f131ae1ff95242f3d74023b0e10b561db1bda918
Martin Zagora <zaggino@gmail.com>
2015-02-12 22:52:50 +1100

    Add config file for ESLint

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-/*global brackets, define, window*/
+/*global define, window*/
 
 define(function (require) {
     "use strict";
-
-    // register es6 as javascript file extension in Brackets
-    var LanguageManager = brackets.getModule("language/LanguageManager");
-    if (LanguageManager.getLanguageForExtension("es6") == null) {
-        LanguageManager.getLanguageForExtension("js").addFileExtension("es6");
-    }
 
     require("dist/browser-polyfill");
     require("dist/main")();
@@ -16,7 +10,7 @@ define(function (require) {
         // TODO: this needs to be populated automatically somehow
         var extensionInfo = JSON.parse(require("text!package.json"));
         window[extensionInfo.name] = {
-            main:   require("dist/main"),
+            main: require("dist/main"),
             class: {
                 Logger: require("dist/class/Logger")
             }
